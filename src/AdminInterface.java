eceb9e7c916bcd8c7c40608475e404dc209f42d6
bahae3 <bahaeddiine12@gmail.com>
2023-12-03 19:40:29 +0100

    I've just started working on this class

--- src/AdminInterface.java
+++ src/AdminInterface.java
@@ -1,7 +1,9 @@
 import javax.swing.*;
 
 public class AdminInterface extends JFrame{
-    public AdminInterface(){
+    int id;
+    public AdminInterface(int id){
+        this.id = id;
         this.setTitle("Interface de l'administrateur");
         this.setVisible(false);
         this.setBounds(460, 200, 700, 500);
